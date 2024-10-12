0f40783c0d892809e5e586582deb9ec9731e5535
ahmed omar <ahmedrefay3000@gmail.com>
2024-10-13 00:06:00 +0300

    add Filter to products route

---
+++ prisma/migrations/20241012200401_add_sizes_and_color/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "products" ADD COLUMN     "color" TEXT,
+ADD COLUMN     "size" TEXT;
--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -4,10 +4,42 @@ import { db } from "../utils/prisma";
 // getAllProduct
 export const getAllProduct = async (req: Request, res: Response) => {
     try {
+        const { name, category, price, color, size } = req.query;
+        console.log(
+            "name",
+            name,
+            "category",
+            category,
+            "price",
+            price as string,
+            "color",
+            color,
+            "size",
+            size
+        );
         const allProduct = await db.product.findMany({
             include: {
                 Sizes: true,
                 colors: true,
+            },
+            where: {
+                title: {
+                    contains: name as string,
+                },
+                category: {
+                    equals: category as string,
+                },
+                price: {
+                    // lte: Number(maxPrice),
+                    // gte: Number(minPrice),
+                    equals: price ? Number(price) : undefined,
+                },
+                color: {
+                    equals: color as string,
+                },
+                size: {
+                    equals: size as string,
+                },
             },
         });
 
